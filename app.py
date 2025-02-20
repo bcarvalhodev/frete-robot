b239fbca348a9c41fd5fba7d2e5eff24ba587051
bcarvalhodev <bcarvalho.dev@gmail.com>
2025-02-20 14:11:36 -0300

    Corrigindo a porta para o Render

--- app.py
+++ app.py
@@ -79,4 +79,5 @@ def index():
     return render_template("index.html", resultado=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Pega a porta do ambiente ou usa 5000 como padrão
+    app.run(host="0.0.0.0", port=port)
