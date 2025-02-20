57e8384d57794120b479eaeb072d6dac71a216eb
bcarvalhodev <bcarvalho.dev@gmail.com>
2025-02-20 14:44:10 -0300

    Corrigindo a porta para o Render

--- build.sh
+++ build.sh
@@ -3,9 +3,17 @@
 # Atualiza os pacotes
 apt-get update && apt-get install -y wget unzip
 
-# Instala o Google Chrome
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+# Baixar o Chrome Portable
+wget https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.76/linux64/chrome-linux64.zip
 
-# Confirma a instalação
-google-chrome --version
+# Descompactar o Chrome
+unzip chrome-linux64.zip
+
+# Adicionar o Chrome no PATH (caso necessário)
+export PATH=$PATH:/chrome-linux64
+
+# Instalar as dependências
+pip install -r requirements.txt
+
+# Confirma a instalação do Google Chrome
+./chrome-linux64/google-chrome-stable --version
